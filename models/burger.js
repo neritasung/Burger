ae864879511d33e8a482a3862dd99dce6c65a9ee
Nerita Sung <neritasung@gmail.com>
2018-06-19 18:13:42 -0500

    fixes bugs

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,9 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burger = {
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
@@ -26,4 +26,4 @@ var cat = {
 };
 
 // Export the database functions for the controller
-module.exports = nurger;
+module.exports = burger;
